21ac3f1d6b056b15b1c3c80d84a3937dcbefff2b
lab314-13@labeng.local <lab314-13@labeng.local>
2020-01-28 16:33:50 +0700

    number 2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,3 +1,19 @@
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main(){
+    
+    ofstream cheerbook("C:\\Users\\lab314-13\\Desktop\\lab9\\lab9-2562-2-kasidej02\\cheerbook_copy.txt");
+    ifstream temp("cheerbook.txt");
+    string text;
+    cheerbook << "-------------------- SOTUS ---------------------\n" ;
+    while (getline(temp,text)){
+        cheerbook << text << "\n";
+    }
+    cheerbook << "-------------------- SOTUS ---------------------\n" ;
+}
+
 //cout << "-------------------- SOTUS ---------------------";
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
