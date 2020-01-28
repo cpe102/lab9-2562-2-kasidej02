a933f1c1402dab6e0891a91275d4396d09b4e159
lab314-13@labeng.local <lab314-13@labeng.local>
2020-01-28 16:34:01 +0700

    number 3

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,29 @@
+#include<fstream>
+#include<string>
+#include<cmath>
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+    int count = 0;
+    float sum=0,ee;
+    string text;
+    ifstream yu("C:\\Users\\lab314-13\\Desktop\\lab9\\lab9-2562-2-kasidej02\\score.txt");
+    while(getline(yu,text)){
+        sum += atof(text.c_str());
+        count++;
+        ee += pow(atof(text.c_str()),2);
+    }
+    
+    cout <<"Number of data = " << count <<"\n";
+    cout <<"Mean = " << sum/count <<"\n";
+    cout << "Standard deviation = "<< sqrt(ee/count - (sum/count)*(sum/count));
+
+}
+
+
+
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
